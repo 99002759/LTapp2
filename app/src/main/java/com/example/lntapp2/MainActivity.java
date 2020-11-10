f9b7e55bb8a6254ff2aa46015a550946a318b332
99002759 <Lokesh.Mullaguri@ltts.com>
2020-11-10 17:53:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lntapp2/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.lntapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName() ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG,"onCreate");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"onpause");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"onresume");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"onstop");
+
+    }
+
+    public void clickHandler(View view) {
+        Log.e(TAG,"clickHandler");
+        Intent hIntent = new Intent(MainActivity.this,HomeActivity.class);
+
+        switch (view.getId()){
+            case R.id.buttonlogin:
+                startHome();
+                break;
+            case R.id.buttoncancel:
+                Intent dialIntent =new Intent(Intent.ACTION_VIEW,  Uri.parse("tel:12345678" ));
+                //"//http://www.google.com"));
+                //new Intent(Intent.ACTION_DIAL, Uri.parse("tel:1234567"));
+                startActivity(dialIntent);
+                break;
+        }
+    }
+
+    private void startHome() {
+        Intent hIntent = new Intent(MainActivity.this, HomeActivity.class);
+        hIntent.putExtra("mykey","loki");
+        int c = add(10,20);
+        startActivity(hIntent);
+
+    }
+
+    private int add(int i, int i1) {
+        return i+i1;
+    }
+}

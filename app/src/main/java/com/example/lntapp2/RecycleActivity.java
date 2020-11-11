f494bdb592b8d454458817a8844d76a3fa22130d
99002759 <Lokesh.Mullaguri@ltts.com>
2020-11-11 15:39:53 +0530

    appications

---
+++ app/src/main/java/com/example/lntapp2/RecycleActivity.java
@@ -0,0 +1,18 @@
+package com.example.lntapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class RecycleActivity extends AppCompatActivity {
+    String[] languagesData;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle);
+        languagesData = new String[]{"english","hindi","urdu","arabic","kannada"};
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        MyAdapter adapter = new MyAdapter(languagesData);
+    }
+}

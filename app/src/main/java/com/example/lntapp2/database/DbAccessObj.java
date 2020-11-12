0e0a4c266bdca70957baa6f5d3d371eb9f616253
99002759 <Lokesh.Mullaguri@ltts.com>
2020-11-12 11:32:43 +0530

    database

---
+++ app/src/main/java/com/example/lntapp2/database/DbAccessObj.java
@@ -0,0 +1,19 @@
+package com.example.lntapp2.database;
+
+/**
+ * thjis class will help me create/read/update/delete a row in the table
+ */
+public class DbAccessObj {
+
+    public DbAccessObj() {
+    }
+
+    private void openDb(){}
+    private void closeDb(){}
+
+    public void createRow(String title, String subtitle){}
+    private void readRow(){}
+    private void updateRow(){}
+    private void deleteRow(){}
+
+}
